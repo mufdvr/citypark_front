ee1b05b539f6ca7e2e17ff07eaf7c2712d78d70a
irondru <belora@nxt.ru>
2018-05-08 01:25:38 +0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+import { Provider } from 'react-redux'
+
+import store, { history } from 'store'
+import { ConnectedRouter } from 'react-router-redux'
+import { renderRoutes } from 'react-router-config'
+
+import routes from 'routes'
+
+const App = () =>
+  <Provider store={store}>
+    <ConnectedRouter history={history}>{renderRoutes(routes)}</ConnectedRouter>
+  </Provider>
+
+
+export default App
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import MainLayout from './components/MainLayout'
+
+const routes = [
+  {
+    path: '/',
+    // exact: true,
+    component: MainLayout,
+    routes: [
+
+    ]
+  }
+]
+
+export default routes
