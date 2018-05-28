4cc5378786f2a4a2265bc3a829d82ac25765fae1
irondru <belora@nxt.ru>
2018-05-28 20:09:12 +0300

    fix

--- src/customer/features/RestaurantAndCafe/containers/NewsShow/NewsShow.js
+++ src/customer/features/RestaurantAndCafe/containers/NewsShow/NewsShow.js
@@ -3,25 +3,40 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import * as actions from '../../actions'
+import { Breadcrumbs } from 'components'
+import { REST_MAIN, NEWS } from '../../routes'
 
 class NewsShow extends React.Component {
 
   componentDidMount = () => {
     const { fetching, showNews, match: { params } } = this.props
-    console.log(this.props);
     !fetching && showNews(params.id)
   }
 
+  xss = target => {   //незабываем фильтровать <script> на бэке
+    const { body } = this.props.news
+    if (target && body) target.innerHTML = body
+  }
+
   render = () => {
+    if (!this.props.news) return <div />
+    const { title, created_at, image } = this.props.news
     return (
       <div className="light">
+        <Breadcrumbs links={[ REST_MAIN, NEWS ]} />
+          <div className="page_date">{created_at}</div>
+          <div className="page_img">
+          	<img src={process.env.REACT_APP_BACK_ROOT + image} />
+          </div>
+          <h1>{title}</h1>
+          <div ref={this.xss} />
       </div>
     )
   }
 }
 
 const mapStateToProps = state => ({
-  newsItem: state.restcafe.payload.news,
+  news: state.restcafe.payload.news,
   fetching: state.restcafe.fetching
 })
 
