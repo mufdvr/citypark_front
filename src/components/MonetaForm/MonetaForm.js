06a06f98cc4e74a92d2f54f94c5b5b9c9708bfb4
mufdvr <belora2010@yandex.ru>
2018-08-21 20:04:08 +0300

    wtf

--- src/components/MonetaForm/MonetaForm.js
+++ src/components/MonetaForm/MonetaForm.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import FromStyles from './MonetaForm.css'
 import { Spinner } from 'components'
 
 class MonetaForm extends React.Component {
+
+  addLink = target => {
+    let cssLink = document.createElement("link")
+    cssLink.href = FromStyles
+    cssLink.rel = "stylesheet"
+    cssLink.type = "text/css"
+    target.contentDocument.head.appendChild(cssLink)
+  }
 
   render = () => {
     const { mntTransactionId, mntAmount, mntSignature, paymentType } = this.props
     const { REACT_APP_MNT_ID, REACT_APP_ASSISTANT, REACT_APP_MNT_TEST_MODE, REACT_APP_MNT_CURRENCY_CODE } = process.env
     return (
-      <iframe onLoad={() => console.log(77777777)} id="widget" style={{width: "100%", height: "700px"}} 
+      <iframe ref={this.addLink} onLoad={() => console.log(77777777)} id="widget" style={{width: "100%", height: "700px"}} 
         src={"https://demo.moneta.ru/assistant.widget?" +
         "MNT_ID=" + REACT_APP_MNT_ID +
         "&MNT_TEST_MODE=" + REACT_APP_MNT_TEST_MODE +
--- src/components/MonetaForm/index.js
+++ src/components/MonetaForm/index.js
@@ -1,3 +1,4 @@
 import MonetaForm from './MonetaForm'
+import './MonetaForm.css'
 
 export default MonetaForm
