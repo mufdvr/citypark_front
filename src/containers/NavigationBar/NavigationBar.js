1fa61a82edaa3cfa2b8b48f95bb01375b3a97ad9
mufdvr <belora2010@yandex.ru>
2018-08-31 14:47:42 +0300

    fixes

--- src/containers/NavigationBar/NavigationBar.js
+++ src/containers/NavigationBar/NavigationBar.js
@@ -36,6 +36,10 @@ class NavigationBar extends React.Component {
     )
   }
 
+  handleClick = () => this.setState({
+    visible: false
+  })
+
   componentWillReceiveProps = (nextProps) => {
     const { user } = nextProps
     const { subMenuVisible } = this.state
@@ -57,81 +61,81 @@ class NavigationBar extends React.Component {
         	<div className="l_grad"></div>
           <div className="r_grad"></div>
           <ul className="mmenu" style={ this.isMobileView ? {left: visible ? 0 : "-300px"} : {}}>
-            <li>
+            <li onClick={this.handleClick}>
               <Link to="/" className="first active">Главная</Link>
             </li>
-            <li onMouseOver={() => this.handleHover(0)} onMouseOut={() => this.handleHover(0)}>
+            <li onClick={this.handleClick} onMouseOver={() => this.handleHover(0)} onMouseOut={() => this.handleHover(0)}>
               <Link to={REST_MAIN.URL}>
                 {REST_MAIN.TITLE}
               </Link>
               <ul className={`sub ${this.state.subMenuVisible[0] ? "fade-in" : "fade-out"}`}>
-                <li>
+                <li onClick={this.handleClick}>
                   <Link to={RESTAURANT.URL}>
                     {RESTAURANT.TITLE}
                   </Link>
                 </li>
-                <li>
+                <li onClick={this.handleClick}>
                   <Link to={CAFE.URL}>
                     {CAFE.TITLE}
                   </Link>
                 </li>
-                <li>
+                <li onClick={this.handleClick}>
                   <Link to={NEWS.URL}>
                     {NEWS.TITLE}
                   </Link>
                 </li>
-                <li>
+                <li onClick={this.handleClick}>
                   <Link to={MENU.URL}>
                     {MENU.TITLE}
                   </Link>
                 </li>
-                <li>
+                <li onClick={this.handleClick}>
                   <Link to={CHEF_BLOG.URL}>
                     {CHEF_BLOG.TITLE}
                   </Link>
                 </li>
               </ul>
             </li>
-            <li onMouseOver={() => this.handleHover(1)} onMouseOut={() => this.handleHover(1)}>
+            <li onClick={this.handleClick} onMouseOver={() => this.handleHover(1)} onMouseOut={() => this.handleHover(1)}>
               <Link to={HOTEL_MAIN.URL}>
                 {HOTEL_MAIN.TITLE}
               </Link>
               <ul className={`sub ${this.state.subMenuVisible[1] ? "fade-in" : "fade-out"}`}>
-                <li>
+                <li onClick={this.handleClick}>
                   <Link to={CATALOG.URL}>
                     {CATALOG.TITLE}
                   </Link>
                 </li>
-                <li>
+                <li onClick={this.handleClick}>
                   <Link to={DOCUMENTATION.URL}>
                     {DOCUMENTATION.TITLE}
                   </Link>
                 </li>
               </ul>
             </li>
-            <li>
+            <li onClick={this.handleClick}>
               <Link to={CONTACTS.URL}>
                 {CONTACTS.TITLE}
               </Link>
             </li>
-            <li onMouseOver={() => this.handleHover(2)} onMouseOut={() => this.handleHover(2)}>
+            <li onClick={this.handleClick} onMouseOver={() => this.handleHover(2)} onMouseOut={() => this.handleHover(2)}>
               <Link to={PERSONAL.URL}>
                 { user && user.id ? user.name : PERSONAL.TITLE }
               </Link>
               {
                 user && user.id ?
                   <ul className={`sub ${this.state.subMenuVisible[2] ? "fade-in" : "fade-out"}`}>
-                    <li>
+                    <li onClick={this.handleClick}>
                       <Link to={FAVORITES.URL}>
                         {FAVORITES.TITLE}
                       </Link>
                     </li>
-                    <li>
+                    <li onClick={this.handleClick}>
                       <Link to={ORDERS.URL}>
                         {ORDERS.TITLE}
                       </Link>
                     </li>
-                    <li>
+                    <li onClick={this.handleClick}>
                       <a onClick={signOut}>Выход</a>
                     </li>
                   </ul>
