137329ab891fc0db79404507bc43d7a02e6dc97b
mufdvr <belora2010@yandex.ru>
2018-09-19 12:46:08 +0300

    refactoring

--- src/components/Delivery/Delivery.js
+++ src/components/Delivery/Delivery.js
@@ -2,25 +2,52 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-import { DELIVERY_PHOTO } from './background'
 import { RestaurantAndCafe } from 'features'
 
-const Delivery = ({ displayType }) =>
-  <Link to={RestaurantAndCafe.links.MENU.URL}>
-    <div className={`deliver_block deliver_block_${displayType}`}>
-      <img
-        alt="pic"
-        className={`deliver-img deliver-img_${displayType}`}
-        src={DELIVERY_PHOTO}
-      />
-      <div className="d_title">
-        Доставка еды
-      </div>
-    </div>
-  </Link>
+import * as images from './images'
 
-Delivery.propTypes = {
-  displayType: PropTypes.string.isRequired
+class Delivery extends React.Component {
+
+  updateDimensions = () => {
+    this.setState({
+      isMobileView: window.innerWidth < 800
+    })
+  }
+
+  componentWillMount = () => {
+    this.updateDimensions()
+  }
+
+  componentDidMount = () => {
+    window.addEventListener("resize", this.updateDimensions)
+  }
+
+  componentWillUnmount = () => {
+    window.removeEventListener("resize", this.updateDimensions)
+  }
+
+  render = () => {
+    const { displayType } = this.props
+    const { isMobileView } = this.state
+    return (
+      <Link to={RestaurantAndCafe.links.MENU.URL}>
+        <div className={`deliver_block deliver_block_${displayType}`}>
+          <img
+            alt="pic"
+            className={`deliver-img deliver-img_${displayType}`}
+            src={ isMobileView ? images.delivery1 : images.delivery2 }
+          />
+          <div className="d_title">
+            Доставка еды
+      </div>
+        </div>
+      </Link>
+    )
+  }
 }
 
-export default Delivery
+  Delivery.propTypes = {
+    displayType: PropTypes.string.isRequired
+  }
+
+  export default Delivery
